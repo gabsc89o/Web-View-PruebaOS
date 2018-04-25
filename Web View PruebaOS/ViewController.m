cde555b2946b998642f87204240fc85bb1a5942a
gabsc89o <user138066@EU249.local>
2018-04-25 17:27:13 +0100

    change1
    Change1

--- Web View PruebaOS/ViewController.m
+++ Web View PruebaOS/ViewController.m
@@ -17,13 +17,42 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    NSString *texto = @"<html><body><h2 align='center'>HTML ESTATICO</h2></body></html>";
+    [_webView loadHTMLString:texto baseURL:nil];
+    //NSURL *url = [NSURL URLWithString:@"http://www.el-nacional.com"];
+    /*NSURLRequest *loadURL = [[NSURLRequest alloc]initWithURL:url];
+    [_webView loadRequest:loadURL];*/
+    //modificar el source de info.plist
+    /*
+     <string>UIInterfaceOrientationLandscapeRight</string>
+     </array>
+     <key>NSAppTransportSecurity</key>
+     <dict>
+     <key>NSExceptionDomains</key>
+     <dict>
+     <key>www.google.es</key>
+     <dict>
+     <key>   NSExceptionAllowsInsecureHTTPLoads</key>
+     <true/>
+     <key>NSIncludesSubdomains</key>
+     <true/>
+     </dict>
+     </dict>
+     </dict>
+     </dict>
+     </plist>
+     */
 }
-
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
 }
 
+-(IBAction)google:(id)sender{
+    NSURL *url = [NSURL URLWithString:@"http://www.google.es"];
+    NSURLRequest *loadURL = [[NSURLRequest alloc]initWithURL:url];
+    [_webView loadRequest:loadURL];
+}
 
 @end
