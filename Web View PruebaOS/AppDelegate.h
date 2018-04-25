cf773afe18350584e6e375fbb9b429dcd044eb90
gabsc89o <user138066@eu249.local>
2018-04-25 14:54:56 +0100

    Initial Commit

---
+++ Web View PruebaOS/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Web View PruebaOS
+//
+//  Created by user138066 on 4/25/18.
+//  Copyright © 2018 user138066. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Web View PruebaOS/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Web View PruebaOS
+//
+//  Created by user138066 on 4/25/18.
+//  Copyright © 2018 user138066. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
